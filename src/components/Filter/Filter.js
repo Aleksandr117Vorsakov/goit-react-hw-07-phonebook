ee08a9c9d4fc3be9ea37853a49b6ef3f6735caaa
Aleksandr117Vorsakov <vorsakov1994@gmail.com>
2022-11-07 20:16:12 +0300

    task

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeFilter } from 'redux/filterSlice';
+import { getFilter } from 'redux/selectors';
 import { Label, Input } from './Filter.styled';
 
-const Filter = ({ filter, onChange }) => {
+const Filter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
+  const handleFilterChange = e => {
+    e.preventDefault();
+    const currentFilter = e.currentTarget.value;
+    dispatch(changeFilter(currentFilter));
+  };
+
   return (
     <Label>
       Find contacts by name
       <Input
         type="text"
         name="filter"
-        value={filter}
-        onChange={onChange}
+        value={filter.value}
+        onChange={handleFilterChange}
       ></Input>
     </Label>
   );
-};
-
-Filter.propTypes = {
-  filter: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
 };
 
 export default Filter;
